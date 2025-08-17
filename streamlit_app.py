dcd034aa7b60bb63a99828a5561d3213798d9c29
Felippedeabreu <brankeeelo@hotmail.com>
2025-08-17 21:31:08 +0000

    att no app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+st.title("Análise de Emoções de Alunos no Ambiente Educacional")
+st.write("Desenvolvido por: Felippe de Abreu")
+
